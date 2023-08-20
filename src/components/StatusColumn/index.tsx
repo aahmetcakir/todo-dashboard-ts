364b7c87779fdbcbac030fb52f44858435d2cb06
Ahmet ÇAKIR <ahmet.cakir1@trendyol.com>
2023-08-20 17:43:12 +0300

    feat: change status column title

--- src/components/StatusColumn/index.tsx
+++ src/components/StatusColumn/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IStatusColumn } from "../../interface/StatusColumn";
 import Card from "../Card";
 import { Plus, Option } from "../icons";
@@ -6,16 +7,43 @@ interface Props {
   column: IStatusColumn;
 }
 function StatusColumn({ column }: Props) {
+  const [colTitleToggle, setColTitleToggle] = useState<boolean>(false);
+
+  const changeColumnTitle = (column: IStatusColumn, value: string) => {
+    column.title = value;
+  };
+
+  const handleKeyDown = (event: any) => {
+    if (event.key == "Enter") {
+      setColTitleToggle(false);
+    }
+  };
+
   return (
     <div className="column">
-      <div className="header">
-        <div className="title">{column.title}</div>
+      <div
+        className="header"
+        onClick={() => {
+          setColTitleToggle(true);
+        }}
+      >
+        {colTitleToggle ? (
+          <input
+            type="text"
+            placeholder={column.title}
+            className="title-input"
+            onChange={(e) => changeColumnTitle(column, e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+        ) : (
+          <div className="title">{column.title}</div>
+        )}
         <div className="actions">
           <Option />
           <Plus />
         </div>
       </div>
-      <div className="todos">
+      <div className="todos" onClick={() => setColTitleToggle(false)}>
         {column.todos.map((todo) => {
           return <Card key={`todo-${todo.id}`} todo={todo} />;
         })}
